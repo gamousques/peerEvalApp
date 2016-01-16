b67bd8c8ebdfc60279f54a2020f345734118fa94
gaston mousques <gamousques@Gastons-MBP.local>
2016-01-16 15:28:55 -0300

    version con listas

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 'use strict';
-
+(function() {
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
+var peerEvalApp = angular.module('peerEvalApp', [ ]);
+
+    peerEvalApp.controller('courseListCtrl', function () {
+      this.courseData = courseList;
+    });
+
+  var courseList = [
+        {'name': 'Is1',
+         'description': 'intro a la is',
+          'instructor': 'gastn'},
+        {'name': 'Is2',
+         'description': 'intro a la is222',
+          'instructor': 'Maartin'},
+        {'name': 'DA1',
+         'description': 'intro al disño',
+          'instructor': 'Nacho'}
+      ];
+
+// config script
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+})();
