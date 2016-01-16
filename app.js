6f6c3044c7eab9b9e3ba4f91c81efd81a3e81e00
gaston mousques <gamousques@Gastons-MBP.local>
2016-01-16 15:46:41 -0300

    version con tabla

--- app.js
+++ app.js
@@ -3,20 +3,20 @@
 // Declare app level module which depends on views, and components
 var peerEvalApp = angular.module('peerEvalApp', [ ]);
 
-    peerEvalApp.controller('courseListCtrl', function () {
-      this.courseData = courseList;
-    });
+peerEvalApp.controller('courseListCtrl', function () {
+  this.courseData = courseList;
+});
 
   var courseList = [
-        {'name': 'Is1',
-         'description': 'intro a la is',
-          'instructor': 'gastn'},
-        {'name': 'Is2',
-         'description': 'intro a la is222',
-          'instructor': 'Maartin'},
-        {'name': 'DA1',
-         'description': 'intro al disño',
-          'instructor': 'Nacho'}
+        {name: 'Is1',
+         description: 'intro a la is',
+          instructor: 'gastn'},
+        {name: 'Is2',
+         description: 'intro a la is222',
+         instructor: 'Maartin'},
+        {name: 'DA1',
+         description: 'intro al disño',
+         instructor: 'Nacho'}
       ];
 
 // config script
